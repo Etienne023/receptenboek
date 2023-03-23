e19b7b4f795144b12459b96d36895c02e282153a
Etienne van Bergeijk <etiennevanbergeijk@gmail.com>
2023-03-23 11:04:35 +0100

    commit

---
+++ site/nav.php
@@ -0,0 +1,11 @@
+<link rel="stylesheet" href="nav.css">
+<header>
+    <div class="logo"><img src="images/images.png"></div>
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">recepten</a></li>
+            <li><a href="#">contact</a></li>
+        </ul>
+    </nav>
+</header>
--- site/recepten.php
+++ site/recepten.php
@@ -19,6 +19,7 @@ $result = mysqli_query($conn, $sql);
 $all_recepten = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -27,13 +28,30 @@ $all_recepten = mysqli_fetch_all($result, MYSQLI_ASSOC);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Recepten Boek</title>
+    <title>Document</title>
 </head>
 
 <body>
-<?php include("nav.php") ?>
-    
-    
+    <?php include("nav.php") ?>
+    <div class="achtergrond_plaatje">
+        <img src="images/28116-6235-mx.jpg" alt="dit plaatje kan niet geladen worden ">
+        <p>recepten</p>
+    </div>
+    <div class="vakje">
+        <h2>recepten</h2>
+        <div class="plaatjes">
+            <?php foreach ($all_recepten as $recept) : ?>
+                <div class="recept">
+                    <img src="images/<?php echo $recept["plaatje"] ?>">
+                    <div class="tekst">
+                        <h3><?php echo $recept["Naam Recept"] ?></h3>
+
+                    </div>
+                </div>
+            <?php endforeach ?>
+        </div>
+
+    </div>
 </body>
 
 </html>
