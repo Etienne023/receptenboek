dac7fffdac742b78c59c17f211d1ecc1e363f522
Etienne van Bergeijk <etiennevanbergeijk@gmail.com>
2023-03-30 08:59:15 +0200

    nav aangepast

--- site/nav.php
+++ site/nav.php
@@ -4,10 +4,8 @@
     <h1>receptenboek</h1>
     <nav>
         <ul class="navbar">
-            <li><a href="#">Home</a></li>
             <li><a href="#">recepten</a></li>
             <li><a href="#">special</a></li>
-            <li><a href="#">contact</a></li>
         </ul>
     </nav>
 </header>
--- site/special.php
+++ site/special.php
@@ -2,7 +2,7 @@
 require "database.php";
 
 
-$sql = "SELECT * FROM `recepten` WHERE Bereidingduur = (SELECT MAX(Bereidingduur) FROM recepten); ";
+$sql = "SELECT * FROM `recepten` WHERE Bereidingduur = (SELECT MAX(Bereidingduur) FROM recepten);";
 
 $result = mysqli_query($conn, $sql);
 
@@ -25,16 +25,20 @@ $makkelijkste = mysqli_fetch_assoc($result);
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="special.css">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="special.css">
 </head>
+
 <body>
 
-<h1>Het recept dat het langste duurt om te maken</h1>
-<?php echo $langsteDuur["bereidingduur"] ?>
+  <h1>Het recept dat het langste duurt om te maken</h1>
+  <?php echo($langsteDuur)?>
+
 </body>
+
 </html>
